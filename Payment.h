d5dbd4ef7610bdfc6354f9468568360bb394a185
Au Duong Tan <6451071069@st.utc2.edu.vn>
2024-10-07 20:10:46 +0700

    Them file Payment.h va file Payment.cpp

---
+++ Payment.h
@@ -0,0 +1,24 @@
+#ifndef PAYMENT_H
+#define PAYMENT_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Payment {
+private:
+    int customerId;            // ID của khách hàng thực hiện thanh toán
+    double ratePerHour;        // Giá tiền tính theo giờ
+    double totalAmount;        // Tổng số tiền cần thanh toán
+    int usageTimeInMinutes;    // Thời gian sử dụng (phút)
+    bool isPaid;               // Trạng thái thanh toán (đã thanh toán hay chưa)
+
+public:
+    Payment(int customerId, double ratePerHour, int usageTimeInMinutes); // ham khoi tao
+    void calculateTotalAmount(); // tinh tien dua tren thoi gian su dung
+    void processPayment(); // thanh toan 
+    void displayPaymentDetails() const; // hien thi chi tiet thanh toan
+    bool isPaymentComplete() const; // kiem tra thanh toan da duoc thuc hien chua
+    void updateUsageTime(int newUsageTime); // cap nhat thoi gian su dung de tinh lai tien
+};
+#endif 

7d3bd89f6fdb2b857d10aaaa226797e279bc0f23
TranMinhTan3110 <tanbeo31102005@gmail.com>
2024-10-09 21:30:11 +0700

    bo sung them

--- CustomerManager.h
+++ CustomerManager.h
@@ -3,6 +3,8 @@
 
 #include<iostream>
 #include<string>
+#include<vector>
+using namespace std;
 class Customer{
     public:
         string name;
@@ -13,7 +15,7 @@ class Customer{
         //khoi tao mặc định
         Customer();
         //khởi tạo có tham số
-        Customer(string name, int id, bool isActive = true, string userName, string passWord);
+      //  Customer(string name, int id, bool isActive = true, string userName, string passWord);
 
 
 };
@@ -22,18 +24,20 @@ class CustomerManager{
         vector<Customer> customers;
     public:
         //tạo tài khoản cho khách hàng
-        void createAccount(string userName, string passWord, int id);
+        void createAccount(int id);
         //them khách hàng vào danh sách
-        void addCustomer();
+        void addCustomer(const string &name, int id);
         //xóa khách hàng ra khởi danh sách
         void removeCustomer(int id);
         //hiển thị thông tin khách hàng
-        void displayCustomer(int id);
+        void displayCustomers();
         // tìm kiếm khách hàng theo id
-        void findCustmer(int id);
+        Customer *findCustomer(int id);
         //kiểm tra trạng thái hoạt động của khách hàng theo id
         bool isActiveCustomer(int id);
         //in ra danh sách khách hàng đang hoạt động
         void printActiveCustomer();
+        //in ra thông tin chi tiết của một khách hàng theo id
+        void printCustomerDetails(int id);
 };
 #endif

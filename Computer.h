e1f2c1e77e0f35d42ae08a546b545cafe2bf9cff
quysutc2 <6451071063@st.utc2.edu.vn>
2024-10-10 22:08:14 +0700

    them vao ham main

---
+++ Computer.h
@@ -0,0 +1,34 @@
+#ifndef COMPUTER_MANAGER_H
+#define COMPUTER_MANAGER_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Computer {
+public:
+    string name; 
+    int id;          
+    bool isAvailable; 
+    int usageTime;
+    Computer(string name, int id, bool isAvailable)
+        : name(name), id(id), isAvailable(isAvailable) {} 
+};
+
+class ComputerManager {
+private:
+    vector<Computer> computers; 
+
+public:
+    void addComputer(const std::string& name, int id, bool isAvasilable); //Thêm 1 máy tính
+    void removeComputer(int id);//Xóa 1 máy tính  
+    void printDetailComputer(int id);//In dữ liệu 1 máy tính                                       
+    void displayComputers() const;//In toàn bộ máy tính                                       
+    Computer* findComputer(int id);//Tìm máy tính theo id  
+    void updateComputerStatus(int id, bool isAvailable);//Cập nhật trạng thái sử dụng
+    void updateUsageTime(int id, int newUsageTime);//Cập nhật thời gian sử dụng
+    vector<Computer> getAvailableComputers() const;//In ra những máy tính đang hoạt động                            
+};
+
+#endif 

7258550456c451bf8eb8c464779b6f84fb9e13bf
Au Duong Tan <6451071069@st.utc2.edu.vn>
2024-10-10 16:12:16 +0700

    Gop branch de tiep tuc viet ham main

---
+++ computer.cpp
@@ -0,0 +1,37 @@
+#include "Computer.h"
+#include <iostream>
+using namespace std;
+
+void ComputerManager::addComputer(const string& name, int id, bool isAvailable) {
+    cout << "Added computer: " << name << " with ID: " << id << endl;
+}
+
+void ComputerManager::removeComputer(int id) {
+    for (auto it = computers.begin(); it != computers.end(); ++it) {
+        if (it->id == id) {
+            computers.erase(it);
+            cout << "Removed computer with ID: " << id << endl;
+            return;
+        }
+    }
+    std::cout << "Computer with ID: " << id << " not found." << endl;
+}
+
+
+void ComputerManager::displayComputers() const {
+    cout << "List of Computers:" << endl;
+    for (const auto& computer : computers) {
+        cout << "ID: " << computer.id << ", Name: " << computer.name
+                  << ", Available: " << (computer.isAvailable ? "Yes" : "No") << endl;
+    }
+}
+
+
+Computer* ComputerManager::findComputer(int id) {
+    for (auto& computer : computers) {
+        if (computer.id == id) {
+            return &computer;
+        }
+    }
+    return nullptr; // Không tìm thấy
+}
